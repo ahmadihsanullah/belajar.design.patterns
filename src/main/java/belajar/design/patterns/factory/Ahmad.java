336ba0a9add23c7cf3604efe6e9f5e2cd3cdc162
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-01-25 21:04:54 +0700

    menambah objek untuk factory

--- src/main/java/belajar/design/patterns/factory/Ahmad.java
+++ src/main/java/belajar/design/patterns/factory/Ahmad.java
@@ -5,10 +5,18 @@ import java.time.LocalDate;
 
 public class Ahmad implements ContentCreator {
 
+    @Override
     public Content createContent(LocalDate localDate) {
-        if(localDate.getDayOfWeek() == localDate.getDayOfWeek().SATURDAY){
-            return new DesignPatters();
-        }
+        if(localDate.getDayOfWeek() == localDate.getDayOfWeek().TUESDAY){
+                    return new DesignPatters();
+                }
         return null;
     }
+
+    // public Content createContent(LocalDate localDate) {
+    //     if(localDate.getDayOfWeek() == localDate.getDayOfWeek().SATURDAY){
+    //         return new DesignPatters();
+    //     }
+    //     return null;
+    // }
 }
--- src/main/java/belajar/design/patterns/factory/Hanif.java
+++ src/main/java/belajar/design/patterns/factory/Hanif.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 public class Hanif implements ContentCreator {
     
     public Content createContent(LocalDate localDate) {
-        if(localDate.getDayOfWeek() == localDate.getDayOfWeek().SATURDAY){
+        if(localDate.getDayOfWeek() == localDate.getDayOfWeek().TUESDAY){
             return new Docker();
         }
         return null;
